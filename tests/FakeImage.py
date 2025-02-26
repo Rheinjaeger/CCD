3ce97a8d40b01922af8c6db7e9264522c9a5fece
JavierAngel0721 <javiert@vt.edu>
2025-02-26 18:03:45 -0500

    reciever.py for image networking completed

--- tests/FakeImage.py
+++ tests/FakeImage.py
@@ -1,8 +1,7 @@
 import numpy as np
 from PIL import Image
 import socket
-import struct
-import time
+
 
 # Image Size
 WIDTH, HEIGHT = 2048, 512
@@ -16,22 +15,23 @@ def generate_fake_image():
     return image_data
 
 # Function to send image data over Ethernet (TCP)
-"""def send_image(image_data, host='127.0.0.1', port=12345):
+def send_image(image_data, host='127.0.0.1', port=40020):
     try:
         # Create a TCP socket
+        server_address = (host, port)
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.connect((host, port))
+            sock.connect(server_address)
             print(f"Connected to {host}:{port}")
 
             # Convert image data to bytes and send
             sock.sendall(image_data.tobytes())
             print("Image data sent successfully.")
     except Exception as e:
-        print(f"Error sending image data: {e}")"""
+        print(f"Error sending image data: {e}")
 
 if __name__ == "__main__":
     # Generate a fake image
     fake_image_data = generate_fake_image()
 
     # Simulate sending the image over Ethernet
-"""    send_image(fake_image_data)"""
+    #send_image(fake_image_data)
---
+++ tests/receiver.py
@@ -0,0 +1,48 @@
+import logging
+import socket
+from PIL import Image
+
+DEF_PORT = 40020
+BUFFER_SIZE = 1024
+WIDTH, HEIGHT = 2048, 512
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(message)s',
+    handlers=[logging.StreamHandler()]
+)
+
+def start_receiver(port=DEF_PORT, filename = "greyscale_img.png"):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(('0.0.0.0', port))
+    server_socket.listen()
+
+    logging.info(f"Receiver started, listening on port {port}...")
+
+    while True:
+        peer_socket, peer_address = server_socket.accept()
+        logging.info(f"Accepted connection from {peer_address}")
+
+        logging.info(f"Receiving image data...")
+        image_data = b""
+
+        while True:
+            data = peer_socket.recv(BUFFER_SIZE)
+            if not data:
+                break
+            image_data += data
+
+        with open(filename, 'wb') as f:
+            f.write(image_data)
+
+        image = Image.frombytes('L', (WIDTH, HEIGHT),image_data)
+        image.save("greyscale_img.png")
+
+        logging.info(f"Image received and saved as {filename}")
+        peer_socket.close()
+
+if __name__ == '__main__':
+    start_receiver(port=DEF_PORT, filename="greyscale_img.png")
+
+
